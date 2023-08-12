8487e430261064373b2f6b455d15abc6e77423d5
HuangWei <market@zuiyue.com>
2023-08-12 21:08:37 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        help();
+        std::process::exit(1);
+    }
+    let command = &args[1];
+
+    match command.as_str() {
+        "install" => {
+            println!("Installing...");
+        },
+        "uninstall" => {
+            println!("Uninstalling...");
+        },
+        "api" => {
+            let json = &args[2];
+            let value: serde_json::Value = serde_json::from_str(json).unwrap();
+            println!("{:#?}", value);
+        },
+        _ => {
+            help();
+            std::process::exit(1);
+        }
+    }
+}
+
+fn help() {
+    let args: Vec<String> = env::args().collect();
+    eprintln!("Usage:");
+    eprintln!("  {} install", args[0]);
+    eprintln!("  {} uninstall", args[0]);
+    eprintln!("  {} api <json>", args[0]);
 }
